7ed8d94338b2a73e3e9a080729c37010b061e1b3
Troels Johnsen <troelsjohnsen@gmail.com>
2014-03-21 22:30:57 +0100

    added slim framework

--- public/index.php
+++ public/index.php
@@ -2,4 +2,16 @@
 
 require '../vendor/autoload.php';
 
-echo "<h1>Hello World!</h1>";
+$app = new \Slim\Slim(array(
+    'debug' => true,
+    'templates.path' => '../app/views',
+));
+
+$app->get('/test', function() use ($app) {
+    $app->render('test.html');
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+$app->run();
